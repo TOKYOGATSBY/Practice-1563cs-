0260123ed718eb60b8ec10a4353c636bbe868945
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-12 08:25:41 +0300

    version 1.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        var visited = new HashSet<string>();
+        int bayanCount = 0;
+
+        for (int i = 0; i < N; ++i)
+        {
+            string store = Console.ReadLine();
+            if (visited.Contains(store))
+            {
+                bayanCount++;
+            }
+            else
+            {
+                visited.Add(store);
+            }
+        }
+
+        Console.WriteLine(bayanCount);
+    }
+}
